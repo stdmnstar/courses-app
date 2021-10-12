b97b68af83f22ab15eb3283c55b64587cf0b62b3
Aliaksandr Tarhonski <stdmnstar@gmail.com>
2021-10-13 00:30:15 +0300

    feat: task3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,6 +9,9 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { LoginModule } from "./features/login/login.module";
 import { CourseModule } from "./features/course/course.module";
 import { RegistrationModule } from "./features/registration/registration.module";
+import { CourseListModule } from "./features/course-list/course-list.module";
+import { TokenInterceptor } from './auth/interceptors/token.interceptor';
+import { WINDOW_PROVIDERS } from './services/window.service';
 
 @NgModule({
   declarations: [
@@ -15,14 +19,20 @@ import { RegistrationModule } from "./features/registration/registration.module"
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
     CoursesModule,
     FontAwesomeModule,
     LoginModule,
     CourseModule,
-    RegistrationModule
+    RegistrationModule,
+    CourseListModule
   ],
-  providers: [],
+  providers: [{
+    provide: HTTP_INTERCEPTORS,
+    useClass: TokenInterceptor,
+    multi: true
+  }, WINDOW_PROVIDERS],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
